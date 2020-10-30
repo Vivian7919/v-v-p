18ec413a7f3fa091640e6dc980b6b320d9597af0
Vivian7919 <vivadan02@gmail.com>
2020-10-31 02:53:23 +0300

    lab8.2-3

---
+++ lab8.2-3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace lab8._2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //8.2
+            int A, B, k;
+            k = 0;
+            A = int.Parse(Console.ReadLine());
+            B = int.Parse(Console.ReadLine());
+            while (A > B)
+            {
+                A = A - B;
+                k += 1;
+            }
+            Console.WriteLine("Количество отрезков В: {0}", k);
+            //8.2
+            while (A - B >= 0)
+            {
+                A = A - B;
+            }
+            Console.WriteLine("Длина незанятой части : {0}", A);
+        }
+    }
+}
