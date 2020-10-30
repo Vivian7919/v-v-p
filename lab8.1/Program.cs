41efe48886f229b96009218bc478e4847a0ca357
Vivian7919 <vivadan02@gmail.com>
2020-10-31 02:29:41 +0300

    lab8.1

---
+++ lab8.1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace lab8._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int k, a;
+            a = int.Parse(Console.ReadLine());
+            k = a / 1024;
+            if (a % 1024 != 0)
+            {
+                a++;
+            }
+            Console.WriteLine("Size = {0}", k);
+        }
+    }
+}
