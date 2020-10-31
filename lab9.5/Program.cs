b178fe546365ee1cf4713d70c8e2709db14a1e9c
Vivian7919 <vivadan02@gmail.com>
2020-10-31 04:45:12 +0300

    lab9.5

---
+++ lab9.5/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace lab9._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int y;
+            Console.WriteLine("Введите год:");
+            y = int.Parse(Console.ReadLine());
+            Console.WriteLine("Hомер столетия: " + ((y - 1) / 1000) + 1);
+        }
+    }
+}
