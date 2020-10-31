f3451c6d8ee65ee72c9e7655a837a482e4f792b2
Vivian7919 <vivadan02@gmail.com>
2020-10-31 03:51:04 +0300

    lab9.1

---
+++ lab9.1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace lab9._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, m;
+            n = int.Parse(Console.ReadLine());
+            m = n / 60;
+            Console.WriteLine(m + " минут");
+        }
+    }
+}
