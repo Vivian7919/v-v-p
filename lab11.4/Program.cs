afcc97a9f604d047342ee02d424f2e7037d0f198
Vivian7919 <vivadan02@gmail.com>
2020-11-10 22:09:35 +0300

    lab11.4

---
+++ lab11.4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace lab11._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите координату x: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите координату y: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            if (x > 0)
+            {
+                if (y > 0)
+                {
+                    Console.WriteLine("Точка в 1 четверти");
+                }
+                else
+                {
+                    Console.WriteLine("Точка в 4 четверти");
+                }
+            }
+            else
+            {
+                if (y > 0)
+                {
+                    Console.WriteLine("Точка во 2 четверти");
+                }
+                else
+                {
+                    Console.WriteLine("Точка в 3 четверти");
+                }
+            }
+        }
+    }
+}
