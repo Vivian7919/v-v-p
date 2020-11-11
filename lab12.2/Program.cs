76ddd905f84fa7a492906c78a19cc09c0b8b6bd8
Vivian7919 <vivadan02@gmail.com>
2020-11-11 06:52:32 +0300

    /

---
+++ lab12.2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace lab12._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string C = Convert.ToString(Console.ReadLine());
+            int N = Convert.ToInt32(Console.ReadLine());
+            int temp = 0;
+            string CC = "C";
+            string[] d = new string[] { "C", "З", "Ю", "В" };
+            for (int i = 0; i < 4; i++)
+            {
+                if (C == d[i])
+                {
+                    temp = i;
+                }
+            }
+            if (C == CC && N == 1)
+            {
+                Console.WriteLine("В");
+            }
+            else
+            {
+                Console.WriteLine(d[(temp - N) % 4]);
+            }
+        }
+    }
+}
