6ec8b43a8697857da8820236725f28a6b8ec46e0
Vivian7919 <vivadan02@gmail.com>
2020-11-19 02:27:42 +0300

    /

---
+++ lab15.1-2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace lab15._1_2
+{
+    class Program
+    {
+        public static void PowerA3(double A, out double B)
+        {
+            B = A * A * A;
+        }
+        public static double Sign(double X)
+        {
+            if (X < 0)
+            {
+                return -1;
+            }
+            if (X == 0)
+            {
+                return 0;
+            }
+            if (X > 0)
+            {
+                return 1;
+            }
+            return 0;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Задание 15.1");
+            double b;
+            for (double i = 4; i < 9; i++)
+            {
+                PowerA3(i, out b);
+                Console.WriteLine($"{i}*{i}*{i} = {b}");
+            }
+            Console.WriteLine("Задание 15.2");
+            Console.Write("Введите A: ");
+            Double A = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите B: ");
+            Double B = Convert.ToDouble(Console.ReadLine());
+            double y = Sign(A) + Sign(B);
+            Console.WriteLine($"Sign(A) + Sign(B) = {y}");
+        }
+
+    }
+}
